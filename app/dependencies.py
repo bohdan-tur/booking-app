27f4b2ae63ab65600c9eb6a13d92f5ef27a3fe9b
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-06-27 21:02:26 +0300

    feat: add CheckRole class and role dependencies for admin and manager

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,7 +6,7 @@ from app.core.security import verify_access_token
 from app.models.user_model import Users
 from fastapi import HTTPException, status
 from sqlalchemy import select
-
+from app.models.role_model import Role
 from app.db.database import get_db
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
@@ -38,3 +38,24 @@ async def get_current_user(
         raise credentials_exception
 
     return user
+
+
+
+
+class CheckRole:
+    def __init__(self, allowed_roles: list[Role]):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, user: Annotated[Users, Depends(get_current_user)]):
+        if user.role not in self.allowed_roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Your role is {user.role.value if hasattr(user.role, 'value') else user.role}. "
+                       f"Allowed roles: {[role.value for role in self.allowed_roles]}"
+            )
+        return user
+
+
+
+allow_admin = CheckRole([Role.admin])
+allow_admin_and_manager = CheckRole([Role.admin, Role.manager])
