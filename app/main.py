4a34801d581e34c1aa7dc000ac51389e19a3b538
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-06-29 18:41:51 +0300

    chore: wire up routers to the main application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
-
+from app.routers import auth, users, rooms, bookings
 
 
 
 app = FastAPI()
+
+
+
+
+
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(rooms.router, prefix="/rooms", tags=["Rooms"])
+app.include_router(bookings.router, prefix="/bookings", tags=["Bookings"])
